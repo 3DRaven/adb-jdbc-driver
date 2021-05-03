db2181968710125b8c8c435d3eb6397eda383bb6
3draven <3draven@gmail.com>
2021-05-03 20:30:41 +0200

    Init maven preparation

--- src/main/java/com/anyqn/adbjdbc/util/ConsumerWithException.java
+++ src/main/java/com/anyqn/adbjdbc/util/ConsumerWithException.java
@@ -1,3 +1,18 @@
+/*******************************************************************************
+ * Copyright 2021 Renat Eskenin
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.  You may obtain a copy
+ * of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ ******************************************************************************/
 package com.anyqn.adbjdbc.util;
 
 import java.util.function.Consumer;
@@ -6,11 +21,11 @@ import java.util.function.Consumer;
 public interface ConsumerWithException<Target, ExObj extends Exception> {
     void accept(Target target) throws ExObj;
 
-    static <Target> Consumer<Target> handlerBuilder(ConsumerWithException<Target, Exception> handlingConsumer) {
+    static <Target> Consumer<Target> handlerBuilder(final ConsumerWithException<Target, Exception> handlingConsumer) {
         return obj -> {
             try {
                 handlingConsumer.accept(obj);
-            } catch (Exception ex) {
+            } catch (final Exception ex) {
                 throw new RuntimeException(ex);
             }
         };
